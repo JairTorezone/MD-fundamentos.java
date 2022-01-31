fea6677c93f0c16b607b7407f06ba002ce0a6c8a
Jair Torezone <jairtorezone@gmail.com>
2022-01-31 18:10:45 -0300

    Set plus Date

---
+++ src/main/java/md/basico/date/Main_Date.java
@@ -0,0 +1,12 @@
+package md.basico.date;
+
+import java.util.Date;
+
+public class Main_Date {
+
+	public static void main(String[] args) {
+		Date now = new Date();
+		System.out.println(now);
+	}
+
+}
